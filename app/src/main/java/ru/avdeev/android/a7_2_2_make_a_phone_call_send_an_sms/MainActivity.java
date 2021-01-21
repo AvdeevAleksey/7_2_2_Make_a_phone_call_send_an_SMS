6fa87945392b93408ac5cfb1e415a52e1c969161
Avdeev Aleksey <avdeev.hexer@yandex.ru>
2021-01-21 22:35:49 +0300

    Calls but doesn't send text messages

--- app/src/main/java/ru/avdeev/android/a7_2_2_make_a_phone_call_send_an_sms/MainActivity.java
+++ app/src/main/java/ru/avdeev/android/a7_2_2_make_a_phone_call_send_an_sms/MainActivity.java
@@ -1,14 +1,79 @@
 package ru.avdeev.android.a7_2_2_make_a_phone_call_send_an_sms;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
 import android.os.Bundle;
+import android.text.SpannableStringBuilder;
+import android.text.style.RelativeSizeSpan;
+import android.view.Gravity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText collEditText;
+    private EditText sendEditText;
+    private static final int MY_PERMISSIONS_REQUEST_CALL_PHONE=11;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        collEditText = findViewById(R.id.collNumberEditText);
+        sendEditText = findViewById(R.id.sendingMessageEditText);
+        final Button collBtn = findViewById(R.id.btnColl);
+        final Button sendBtn = findViewById(R.id.btnSend);
+        collBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (collEditText.getText().toString().length()==12) {
+                    if (ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.CALL_PHONE)!= PackageManager.PERMISSION_GRANTED) {
+                        // Разрешение не получено
+                        // Делаем запрос на добавление разрешения звонка
+                        ActivityCompat.requestPermissions(MainActivity.this, new String[] {Manifest.permission.CALL_PHONE},MY_PERMISSIONS_REQUEST_CALL_PHONE);
+                    } else {
+                        // Разрешение уже получено
+                        Intent intent = new Intent(Intent.ACTION_CALL,Uri.parse("tel:" + collEditText.getText().toString()));
+                        // Звоним
+                        startActivity(intent);
+                    }
+                } else {
+                    showMyMessage(R.string.No_phone_number, MainActivity.this);
+                }
+            }
+        });
+
+        sendBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (sendEditText.getText().toString().length()>0 && collEditText.toString().length()>0) {
+                    sendTextMessage(collEditText.getText().toString(), sendEditText.getText().toString());
+                } else {
+                    showMyMessage(R.string.There_is_no_text_to_send, MainActivity.this);
+                }
+            }
+        });
+    }
+
+    private void sendTextMessage(String phoneNumber, String message) {
+
+    }
+
+    public static void showMyMessage(int massage, Context context) {
+        String text = context.getString(massage);
+        SpannableStringBuilder biggerText = new SpannableStringBuilder(text);
+        biggerText.setSpan(new RelativeSizeSpan(1.35f), 0, text.length(), 0);
+        Toast toast = Toast.makeText(context, biggerText, Toast.LENGTH_LONG);
+        toast.setGravity(Gravity.CENTER_VERTICAL, 0, 0);
+        toast.show();
     }
 }
