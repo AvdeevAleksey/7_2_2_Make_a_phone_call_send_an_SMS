72b20970f20ba71c007454ba4433b822990be10d
Avdeev Aleksey <avdeev.hexer@yandex.ru>
2021-01-21 21:05:32 +0300

    Initial commit

---
+++ app/src/main/java/ru/avdeev/android/a7_2_2_make_a_phone_call_send_an_sms/MainActivity.java
@@ -0,0 +1,14 @@
+package ru.avdeev.android.a7_2_2_make_a_phone_call_send_an_sms;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
---
+++ app/src/test/java/ru/avdeev/android/a7_2_2_make_a_phone_call_send_an_sms/ExampleUnitTest.java
@@ -0,0 +1,17 @@
+package ru.avdeev.android.a7_2_2_make_a_phone_call_send_an_sms;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+public class ExampleUnitTest {
+    @Test
+    public void addition_isCorrect() {
+        assertEquals(4, 2 + 2);
+    }
+}
